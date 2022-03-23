9b590804be668bec8fb05146404727ba3167b8b5
akhmadjon-dev <info.ahmadjon@gmail.com>
2022-03-23 20:18:32 +0500

    removed and structured

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterReducer from './counterSlice';
 
-export const store = configureStore({
+export default  configureStore({
   reducer: {
     counter: counterReducer,
   },
