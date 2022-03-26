0e81db9a1e0f04ca4890cf3f17f1a084a1c4a255
akhmadjon-dev <info.ahmadjon@gmail.com>
2022-03-26 10:14:51 +0500

    routes formatted

--- src/containers/routes.js
+++ src/containers/routes.js
@@ -1,63 +1,62 @@
-import { lazy } from "react"
-import {IoIosList, IoIosLogIn, IoIosLogOut} from "react-icons/io"
-import {MdDashboard, MdSettings} from "react-icons/md"
-import { Navigate } from "react-router-dom"
+import { lazy } from "react";
+import { IoIosList } from "react-icons/io";
+import { MdDashboard } from "react-icons/md";
+import { Navigate } from "react-router-dom";
 
-const SignIn = lazy(() => import("./Auth/signIn"))
-const SignUp = lazy(() => import("./Auth/signUp"))
-const Home = lazy(() => import("./Dashboard"))
-const Products = lazy(() => import("./Products"))
-const Profile = lazy(() => import("./Profile"))
-
+const SignIn = lazy(() => import("./Auth/signIn"));
+const SignUp = lazy(() => import("./Auth/signUp"));
+const Home = lazy(() => import("./Dashboard"));
+const Products = lazy(() => import("./Products"));
+const Profile = lazy(() => import("./Profile"));
 
 const routes = {
-    public: [
-        {
-            path: '/sign-in',
-            element: SignIn,
-            allowedRoles: ['admin', 'user'],
-            title: 'Sign In',
-        },
-        {
-            path: '/sign-up',
-            element: SignUp,
-            allowedRoles: ['admin', 'user'],
-            title: 'Sign Up',
-        },
-        {
-            path: '*',
-            element: <Navigate to="/sign-in" replace />,
-        }
-    ],
-    private: [
-        {
-            path: '/',
-            element: Home,
-            allowedRoles: ['admin', 'user'],
-            title: 'Home',
-            icon: <MdDashboard />,
-            inHeader: true,
-        },
-        {
-            path: '/products',
-            element: Products,
-            allowedRoles: ['admin', 'user'],
-            title: 'Products',
-            icon: <IoIosList />,
-            inHeader: true,
-        },
-        {
-            path: '/profile',
-            element: Profile,
-            allowedRoles: ['admin', 'user'],
-            title: 'Profile',
-            inHeader: false,
-        },
-        {
-            path: '*',
-            element: <Navigate to="/" replace />, 
-        }
-    ]
-}
+  public: [
+    {
+      path: "/sign-in",
+      element: SignIn,
+      allowedRoles: ["admin", "user"],
+      title: "Sign In",
+    },
+    {
+      path: "/sign-up",
+      element: SignUp,
+      allowedRoles: ["admin", "user"],
+      title: "Sign Up",
+    },
+    {
+      path: "*",
+      element: <Navigate to="/sign-in" replace />,
+    },
+  ],
+  private: [
+    {
+      path: "/",
+      element: Home,
+      allowedRoles: ["admin", "user"],
+      title: "Home",
+      icon: <MdDashboard />,
+      inHeader: true,
+    },
+    {
+      path: "/products",
+      element: Products,
+      allowedRoles: ["admin", "user"],
+      title: "Products",
+      icon: <IoIosList />,
+      inHeader: true,
+    },
+    {
+      path: "/profile",
+      element: Profile,
+      allowedRoles: ["admin", "user"],
+      title: "Profile",
+      inHeader: false,
+    },
+    {
+      path: "*",
+      element: <Navigate to="/" replace />,
+    },
+  ],
+};
 
-export default routes
+export default routes;
